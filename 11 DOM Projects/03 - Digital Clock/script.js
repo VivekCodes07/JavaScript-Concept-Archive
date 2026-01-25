80b981b78538335009a8bef664d639c22ff98c65
Vivek Kr <vivekkrdas03@gmail.com>
2026-01-25 17:38:41 +0530

    Create digital clock using DOM manipulation

---
+++ 11 DOM Projects/03 - Digital Clock/script.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector('#clock');
+const date = new Date;
+
+console.log(date.toLocaleTimeString())
+
+setInterval(function() {
+    const date = new Date;
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
